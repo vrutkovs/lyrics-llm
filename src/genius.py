0470ac054b7e1ccb1178989f4d7e3be23762114b
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-03-25 15:16:25 +0100

    Set user agent

--- src/genius.py
+++ src/genius.py
@@ -8,7 +8,7 @@ load_dotenv()
 token = os.getenv("GENIUS_ACCESS_TOKEN")
 if token is None:
     raise ValueError("GENIUS_ACCESS_TOKEN environment variable is not set")
-genius = lyricsgenius.Genius(token)
+genius = lyricsgenius.Genius(token, user_agent="Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0")
 
 MAX_SONGS = os.getenv("MAX_SONGS") or 10
 
